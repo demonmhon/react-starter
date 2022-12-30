b99b530e3a47155cc09a88e9c2faec6a0221cee7
thanawat.petchsri <thanawat.petchsri@gmail.com>
2022-12-30 16:02:02 +0700

    Update packages, JEST config

--- jest.config.js
+++ jest.config.js
@@ -16,7 +16,6 @@ module.exports = {
   testEnvironment: 'jsdom',
   testMatch: ['**/__tests__/**/*.js?(x)', '**/?(*.)+(spec|test).js?(x)'],
   testPathIgnorePatterns: ['\\\\node_modules\\\\'],
-  testURL: 'http://localhost',
   transformIgnorePatterns: ['<rootDir>/node_modules/'],
   verbose: true
 };
