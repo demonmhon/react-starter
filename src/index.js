1aabac759c865c546c5694a3e0cadadda46847f1
demonmhon <thanawat.petchsri@gmail.com>
2023-04-22 13:24:22 +0700

    Test adding @demonmhon/dx-components

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
+import '@demonmhon/dx-components'
 
 import App from './app';
 import { loadConfig } from '@core/config';
 import './css/styles.css';
+import '@demonmhon/dx-components/dist/themes/themes.css';
 
 const reactApp = async () => {
   await loadConfig();
