a66c67cc31e1e1535d0c8e0f9b1006935f03bca9
demonmhon <thanawat.petchsri@gmail.com>
2025-01-26 21:10:29 +0700

    Update, change language by query string

--- src/route.test.jsx
+++ src/route.test.jsx
@@ -5,6 +5,22 @@ import '@testing-library/jest-dom';
 
 import { AppRouter } from './route';
 
+jest.mock('react-i18next', () => ({
+  // this mock makes sure any components using the translate hook can use it without a warning being shown
+  useTranslation: () => {
+    return {
+      t: (i18nKey) => i18nKey,
+      i18n: {
+        changeLanguage: () => new Promise(() => {}),
+      },
+    };
+  },
+  initReactI18next: {
+    type: '3rdParty',
+    init: () => {},
+  },
+}));
+
 // app.test.js
 it('Render home page', () => {
   render(<AppRouter />, { wrapper: MemoryRouter });
