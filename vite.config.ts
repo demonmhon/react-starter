1d0392d44a8e6c5687bff5e83ae10a3a796b0018
Thanawat Petchsri <thanawat.petchsri@gmail.com>
2026-06-03 14:14:03 +0700

    feat: add docker build, fix vite.config for build

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'node:path'
 export default defineConfig({
   plugins: [
      esmExternalRequirePlugin({
-      external: ['react', /^node:/],
+      external: [/^node:/],
     }),
   ],
   resolve: {
